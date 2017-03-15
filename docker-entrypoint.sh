f22b312e85eb42a6d4b5a261fd416ae0b48eb397
David Santamaría <dsantamaria@keedio.com>
2017-03-15 16:59:14 +0100

    Set docker-entry

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,14 @@ echo "Setting seeds to be $( hostname -I)/"
 
 sed -i "s/%%host_ip%%/$(hostname -I)/g" /opt/apache-cassandra/conf/cassandra.yaml
 
+
 export CLASSPATH=/kubernetes-cassandra.jar
+
+
+mkdir -p /var/lib/cassandra/data
+mkdir -p /var/lib/cassandra/commitlog
+mkdir -p /var/lib/cassandra/saved_caches
+
 
 if [ -n "$CLUSTER_NAME" ]; then
     sed -i 's/${CLUSTER_NAME}/'$CLUSTER_NAME'/g' /opt/apache-cassandra/conf/cassandra.yaml
