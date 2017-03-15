6cde2aea9f82c942b8f5e702ae13b4ab716b15cb
David Santamaría <dsantamaria@keedio.com>
2017-03-15 11:53:07 +0100

    Exec as sudo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -51,7 +51,7 @@ if [ -n "$HELP" ]; then
   exit 0
 fi
 
-
+sudo su -
 # set the hostname in the cassandra configuration file
 sed -i 's/${HOSTNAME}/'$HOSTNAME'/g' /opt/apache-cassandra/conf/cassandra.yaml
 
